272200848367aeed306cee2ea83da787bd361662
Micheal Aaron <michealaaron2000@gmail.com>
2024-01-08 22:14:07 +0530

    Issue Fixed

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,10 +10,10 @@ const Main = () => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
-            LET&#39;S BUILD SOMETHING TOGETHER
+            LET'S BUILD SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Aaron</span>
+            Hi, I'm <span className='text-[#5651e5]'> Aaron</span>
           </h1>
           <h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
